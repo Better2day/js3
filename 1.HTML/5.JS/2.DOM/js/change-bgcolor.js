ef30296b4ab2b06b2f83ad2d5a3599b1569a3759
Joon <wushu80@hanmail.net>
2025-11-19 22:46:45 +0900

    [과제] 간헐적 오류 (배경색 변경 실패) 수정 - 오류: 버튼 클릭시 배경 색상이 변경되도록 만들었는데, 변경되지 않을 때가 있음 - 원인: R, G, B 각각의 값에 10진수 랜덤 숫자를 할당한 다음에 RGB 전체를 16진수로 변환해서 색상코드를 만들었는데, 랜덤 숫자 중에 하나라도 한 자리 숫자가 있으면 16진수로 변환한 결과가 정상적인 6자리가 아니라 5자리 이하가 돼서 잘못된 색상코드가 생성됨 - 해결: R, G, B 랜덤 값을 각각 2자리로 패딩 처리함으로써, 랜덤 숫자가 한 자리 가 나오더라도 변환한 16진수 색상코드는 항상 6자리가 되도록 처리

--- 1.HTML/5.JS/2.DOM/js/change-bgcolor.js
+++ 1.HTML/5.JS/2.DOM/js/change-bgcolor.js
@@ -82,20 +82,31 @@ function randomBGColor() {
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
-  let redHex = Math.floor(Math.random() * 256).toString(16);
-  let greenHex = Math.floor(Math.random() * 256).toString(16);
-  let blueHex = Math.floor(Math.random() * 256).toString(16);
+  // 2. 오류 해결: 16진수 색상코드의 R, G, B 별로 패딩 처리(R, G, B 각각 2자리, 합쳐서 6자리를 유지하도록)해서 해결
+  let redHex = red.toString(16).padStart(2, '0');
+  let greenHex = green.toString(16).padStart(2, '0');
+  let blueHex = blue.toString(16).padStart(2, '0');
   // 랜덤 배경 색상이 색상 코드 레이어의 테두리와 같은 색이면 테두리가 안 보이게 되므로, 테두리 색은 항상 랜덤 색상의 보색으로 설정
   let redComp = 255 - red;
   let greenComp = 255 - green;
   let blueComp = 255 - blue;
+  console.log(`bg = (${red}, ${green}, ${blue}) / bgHex = #${redHex}${greenHex}${blueHex}`);
   // 가시성에만 초점을 두면, 보색이 아니라 그냥 50~100 정도를 더해도 별 차이 없을 것 같다. (단, 더한 게 255를 넘는 경우, modulus 처리)
   // let redComp = (red + 100) % 255;
   // let greenComp = (green + 100) % 255;
   // let blueComp = (blue + 100) % 255;
 
+  // document.body.style = `background-color: rgb(${red}, ${green}, ${blue})`; // 오류 발생하지 않음
+  // document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; // 오류 발생하지 않음
+  // document.body.style.backgroundColor = `#a5f25`; // 오류 발생 → 랜덤 색상 적용 안 됨 (오류 메시지가 안 나와서 디버깅 더 어려움!)
+  // 1. 가끔씩 발생하는 오류의 원인 발견:
+  // 아래 16진수 색상코드의 문자열 길이가 6자리 미만이면, 버튼 클릭시 body style에 들어가는 색상코드가 변경되지 않는다.
+  // JS 소스코드에 '16진수' 색상코드를 넣어도, 클릭 이벤트 발생 후 이 함수가 호출된 후에
+  // body.style이 변경될 때 브라우저가 '10진수'를 사용하는 rgb() 형태로 자동 변환한 후 렌더링하는 것 같은데,
+  // 16진수 색상코드의 문자열 길이가 6자리 미만이면 자동 변환이 되지 않는 것을 확인 (개발자 도구 → Element 탭에서 확인)
   document.body.style.backgroundColor = `#${redHex}${greenHex}${blueHex}`;
   // document.getElementById('RGBCode').style.display = 'block';
   document.getElementById('RGBCode').style = `border: 1px solid rgb(${redComp}, ${greenComp}, ${blueComp})`;
   document.getElementById('RGBCode').innerHTML = `RGB(${red}, ${green}, ${blue})<BR>#${redHex}${greenHex}${blueHex}`;
+  // return 0;
 }
