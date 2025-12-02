d72dd6e722746539bf57aa9385f53cf65794860b
Joon <wushu80@hanmail.net>
2025-12-03 04:53:39 +0900

    [자습] (배열 고차 함수 등) 화살표 함수에서 객체를 반환할 때 잘 발생하는 오류의 원인과 해결책

--- 2.NODE/2.CLASS/13.shoppingmall/shop/Order.js
+++ 2.NODE/2.CLASS/13.shoppingmall/shop/Order.js
@@ -46,6 +46,13 @@ class Order {
         quantity
       }))
     }
+
+    // 아래 map 함수에서 객체를 반환하는데, 일반 콜백 함수처럼 화살표 뒤에 { } 블록을 쓰고 그 안에 객체 리터럴을 넣으면 오류 발생
+    // MDN 화살표 함수 주의사항 - https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+    // "간결한 본문 구문은 (params) => { object: literal }을 사용하여 객체 리터럴을 반환하면 예상대로 작동하지 않습니다.
+    // JS는 화살표 뒤에 오는 토큰이 왼쪽 중괄호 {가 아닌 경우에만 화살표 함수에 간결한 본문이 있는 것으로 간주합니다.
+    // 중괄호 {} 안의 코드는 일련의 구문으로 파싱되며, 여기서 foo는 객체 리터럴의 키가 아닌 label이 됩니다.
+    // ※ 해결책: 이 문제를 해결하려면 객체 리터럴을 괄호로 묶으면 됩니다.
     // const items = [];
     // items.push(this.products.map(({ product, quantity }) => ({
     //   name: product.name,
