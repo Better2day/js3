1c46fc9ffc5ed800d090924bfaba9faadfcf89e1
Joon <wushu80@hanmail.net>
2025-12-04 15:40:11 +0900

    [구조] 어제 수업 코드 변경 (minor)

--- 2.NODE/9.PROJECT/2.crmgen.js
+++ 2.NODE/9.PROJECT/2.crmgen.js
@@ -16,22 +16,20 @@ function generateGender() {
 }
 
 function generateGender2() {
-  return (prob < 0.5) ? '남성' : '여성';
-
+  return Math.random() < 0.5 ? '남성' : '여성';
 }
 
 function generateBirthdate() {
   const year = Math.floor(Math.random() * 40) + 1980; // 0 ~ 39 + 1980 = 1980 ~ 2019
   const month = Math.floor(Math.random() * 12) + 1; // 0 ~ 11 + 1 = 1 ~ 12
-  const day = Math.floor(Math.random() * 30 + 1); // 0 ~ 29 + 1 = 1 ~ 30
+  const day = Math.floor(Math.random() * 28 + 1); // 0 ~ 29 + 1 = 1 ~ 30
   // 윤달 같은 것 처리하려면 아래처럼. 쉽게 하고 싶으면 일자는 0 ~ 28 로 고정해도 된다.
-  if (month == 2) {
-
-  }
-
+  // if (month == 2) {
+  // }
 
   return `${year}-${month}-${day}`;
 }
 
-console.log(generateGender());
+console.log(generateName());
 console.log(generateGender2());
+console.log(generateBirthdate());
