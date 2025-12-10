55c34bb69a524d1939992806a3be836769d36bb4
Joon <wushu80@hanmail.net>
2025-12-10 20:07:49 +0900

    [Class] Node.js Express Web Server - CRUD practice - Async Await - Axios

---
+++ 1.HTML/5.JS/7.AXIOS/1.axios.js
@@ -0,0 +1,26 @@
+// 즉시 실행 함수 (IIFE: Immediately Invoked Function Expressionn)
+// 1. 원래 fetch로 요청하기
+// (async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//   const data = await res.json();
+//   console.log(data);
+// })();
+
+async function fetch_main() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const data = await res.json();
+  console.log(data);
+};
+
+// fetch_main();
+
+
+// 2. axios로 요청하기
+const axios = require('axios');
+async function axios_main() {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+  const data = res.data;
+  console.log(data);
+}
+
+axios_main();
