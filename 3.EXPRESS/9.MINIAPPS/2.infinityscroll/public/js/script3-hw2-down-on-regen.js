271bc87f20473ba14f1a9e4db9dc5a170a432882
Joon <wushu80@hanmail.net>
2025-12-13 16:37:15 +0900

    [Comment] Infinity Scroll 주석 추가 정리

--- 3.EXPRESS/9.MINIAPPS/2.infinityscroll/public/js/script3-hw2-down-on-regen.js
+++ 3.EXPRESS/9.MINIAPPS/2.infinityscroll/public/js/script3-hw2-down-on-regen.js
@@ -30,7 +30,11 @@ async function getNextItems() {
   result.appendChild(fragment);
 
   // 새로 다음(하단) 품목을 읽어왔으므로 추가한 품목 개수만큼,
-  // 다음 번 getNextItems() 함수 호출시 사용할 nextStart/nextEnd 배열 인덱스 증가
+  // 다음 번 getNextItems() 함수 호출시 사용할 nextStart/nextEnd 배열 인덱스 범위를 뒤로 옮긴다.
+  // getPrevItems와 다르게 fetch 후에 설정하는 이유는,
+  // 백엔드에서 일정 주기로 품목이 무작위로 생성되고 있어서, 일단 다음 품목을 읽어와야 하기 때문이다.
+  // (실제 시스템도 품목 추가/삭제가 발생하므로 마찬가지 상황)
+  // 결과 레이어 하단 아이템의 품번은 다음 로딩과 무관하고, 무조건 읽기 시도 후 반환 품목에 따라서 다음 로드 범위를 조절한다.
   setNextRange();
 
   console.log('현재 화면에 보이는 품목 전체 개수: ', result.children.length);
@@ -40,11 +44,14 @@ async function getNextItems() {
     removePrevItems(itemQtyToRemove);
     // 다음 번 getPrevItems() 함수 호출에 사용할 배열 인덱스 범위 조절
     // ∵ maxItemsOnScreen 개수를 초과하는만큼 상단 품목을 삭제했으므로,
-    // 삭제한 개수만큼 다음 load에 사용할 prevStart/prevEnd 값도 감소시켜야 중간에 빠지는 품목이 생기지 않는다.
+    // 삭제한 개수만큼 prevStart/prevEnd 배열 인덱스 범위를 뒤로 옮겨놓아야
+    // 다음에 이전 품목 로딩시 중간에 누락되는 품목이 생기지 않는다.
     setPrevRange();
   }
   console.log('getNextItems() 함수 끝');
 }
+
+// ※ 스크롤이 발생해서 한쪽 끝에 도달하면, 양방향 모두 다음 번에 로딩할 배열 인덱스 범위를 조절해야 한다.
 
 async function getPrevItems() {
   console.log('getPrevItems() 함수 시작');
@@ -54,6 +61,7 @@ async function getPrevItems() {
   setPrevRange();
 
   // 화면 최상단에 이미 첫 품목이 있으면 이전 품목 로드 작업 중단 (fetch로 발생하는 network, disk I/O 부하 제거)
+  // getNextItems와 다르게 fetch 전에 setPrevRange() 함수를 통해서 배열 인덱스를 설정하는 이유이다.
   if (prevEnd == 0) {
     return false;
   }
@@ -75,8 +83,8 @@ async function getPrevItems() {
   if (itemQtyToRemove > 0) {
     removeNextItems(itemQtyToRemove);
     // 다음 번 getNextItems() 함수 호출에 사용할 배열 인덱스 범위 조절
-    // ∵ maxItemsOnScreen 개수를 초과하는만큼 하단 품목을 삭제했으므로,
-    // 삭제한 개수만큼 다음 load에 사용할 nextStart/nextEnd 값도 감소시켜야 중간에 빠지는 품목이 생기지 않는다.
+    // 삭제한 개수만큼 nextStart/nextEnd 배열 인덱스 범위를 앞으로 옮겨놓아야
+    // 다음에 다음 품목 로딩시 중간에 누락되는 품목이 생기지 않는다.
     setNextRange();
   }
 }
@@ -95,7 +103,7 @@ function removeNextItems(itemQtyToRemove) {
   }
 }
 
-// getPrevtems() 함수 호출시 사용할 prevStart/prevEnd 배열 인덱스 설정
+// getPrevtems() 함수 호출시 사용할 prevStart/prevEnd 배열 인덱스 범위 설정
 function setPrevRange() {
   // 배열 인덱스 범위 설정중, 검색할 시작/끝 인덱스가 0 미만이 되는 일을 예방(제한)하기 위해 max 함수 사용
   prevEnd = Math.max(0, parseInt(result.firstElementChild.textContent.split(' ').slice(-1)) - 1);
@@ -103,16 +111,17 @@ function setPrevRange() {
   console.log(`prevStart: ${prevStart} / prevEnd: ${prevEnd}`);
 }
 
-// getNextItems() 함수 호출시 사용할 nextStart/nextEnd 배열 인덱스 설정
+// getNextItems() 함수 호출시 사용할 nextStart/nextEnd 배열 인덱스 범위 설정
 function setNextRange() {
   nextStart = parseInt(result.lastElementChild.textContent.split(' ').slice(-1));
   nextEnd = nextStart + itemsPerLoad;
   console.log(`nextStart: ${nextStart} / nextEnd: ${nextEnd}`);
 }
 
+// 부모 노드에 자식 노드 추가
 function appendChildToParent(parent, child) {
   const itemElement = document.createElement('div');
-  itemElement.classList.add('item'); // 디자인을 넣기 위해서 클래스 추가
+  itemElement.classList.add('item');
   itemElement.textContent = child;
   parent.appendChild(itemElement);
 }
@@ -121,7 +130,7 @@ function appendChildToParent(parent, child) {
 document.addEventListener('DOMContentLoaded', () => {
   console.log("DOM ready");
 
-  getNextItems();
+  getNextItems(); // 웹페이지 접속시 itemsPerLoad 개수만큼 초기 품목 읽어오기
 });
 
 window.addEventListener('scroll', () => {
