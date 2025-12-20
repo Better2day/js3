4f845bc5caf10e26ce8a217ca7b590deabdba83c
Joon <wushu80@hanmail.net>
2025-12-21 00:28:25 +0900

    [Misc] Rename DB and table name

--- 4.DATABASE/9.MINIAPPS-DB/5.board/init-db.sql
+++ 4.DATABASE/9.MINIAPPS-DB/5.board/init-db.sql
@@ -1,19 +1,19 @@
 -- sqlite3 board.sqlite < init_database.sql
-DROP TABLE IF EXISTS board;
+DROP TABLE IF EXISTS memo;
 
-CREATE TABLE board (
+CREATE TABLE memo (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(50) NOT NULL,
     message VARCHAR(200)
 );
 
-INSERT INTO board(title, message) VALUES
+INSERT INTO memo(title, message) VALUES
   ('title1', 'message1'),
   ('title2', 'message2'),
   ('title3', 'message3'),
   ('title4', 'message4');
 
--- INSERT INTO board(title, message) VALUES('title1', 'message1');
--- INSERT INTO board(title, message) VALUES('title2', 'message2');
--- INSERT INTO board(title, message) VALUES('title3', 'message3');
--- INSERT INTO board(title, message) VALUES('title4', 'message4');
+-- INSERT INTO memo(title, message) VALUES('title1', 'message1');
+-- INSERT INTO memo(title, message) VALUES('title2', 'message2');
+-- INSERT INTO memo(title, message) VALUES('title3', 'message3');
+-- INSERT INTO memo(title, message) VALUES('title4', 'message4');
