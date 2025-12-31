c2b617308fda192f8240de2012d3a3ffb28b7520
Joon <wushu80@hanmail.net>
2026-01-01 04:55:38 +0900

    [Project] CRM users page - basic functionality

--- 5.PROJECT/1.CRM/public/scripts/users.js
+++ 5.PROJECT/1.CRM/public/scripts/users.js
@@ -1,14 +1,45 @@
 document.addEventListener('DOMContentLoaded', getUsers);
 
-function getUsers() {
-  // BE에 users page 요청
-  const users = fetch('/users');
+async function getUsers() {
+  const res = await fetch('/api/users');
+  const users = await res.json();
+
   if (users) {
-    addUsers();
+    renderUsers(users);
   }
 };
 
-function addUsers() {
-  // table → tbody 안에 사용자 추가
+function renderUsers(users) {
+  const userTable = document.getElementById('user-table');
+  const userTheadTr = userTable.querySelector('thead > tr');
+  const userTbody = userTable.querySelector('tbody');
 
+  if (users.length > 0) {
+    // table → thead 안에 사용자 데이터 헤더 추가
+    const tableHeaders = Object.keys(users[0]);
+    tableHeaders.forEach(header => {
+      if (header != 'Address') {
+        const th = document.createElement('th');
+        th.textContent = header;
+        userTheadTr.appendChild(th);
+      }
+    })
+
+    // table → tbody 안에 사용자 데이터 추가
+    users.forEach(user => {
+      const tr = document.createElement('tr');
+      // console.log(Object.entries(user));
+
+      Object.entries(user).forEach(([key, val]) => {
+        if (key != 'Address') {
+          const td = document.createElement('td');
+          td.textContent = val;
+          tr.appendChild(td);
+        }
+      })
+      userTbody.appendChild(tr);
+    })
+  } else {
+    userTbody.innerHTML = '<tr><td>사용자 데이터가 없습니다.</td></tr>';
+  }
 };
