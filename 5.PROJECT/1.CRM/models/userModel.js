cf3da83a047cbe2d29acac73708ba0289855a446
Joon <wushu80@hanmail.net>
2026-01-04 00:49:21 +0900

    feat(users): set up basic scaffolding for search
    - BE(userModel.js): update DB query for search
    - FE(users.html): add basic search form (name, gender)
    - tested search via direct URL query parameter
      (form-based search to be added soon)

--- 5.PROJECT/1.CRM/models/userModel.js
+++ 5.PROJECT/1.CRM/models/userModel.js
@@ -10,19 +10,14 @@ function getUsers({ name, gender, page = 1 }) {
 
   const rows = db.prepare(`
     SELECT *
-      FROM users
-     WHERE name LIKE ?
-     LIMIT 20
-     OFFSET ?
-     `).all(`%${name}%`, (page - 1) * PAGE_SIZE);
-  //  `).all(name, (page - 1) * PAGE_SIZE);
+    FROM users
+    WHERE name LIKE ?
+    AND gender LIKE ?
+    LIMIT ?
+    OFFSET ?
+     `).all(`%${name}%`, `%${gender}%`, PAGE_SIZE, (page - 1) * PAGE_SIZE);
 
   console.log(rows);
-
-  // WHERE name LIKE '%?%'
-  //  AND gender = ?
-
-  // `).all(name, gender, (page - 1) * PAGE_SIZE);
 
   return rows;
 }
@@ -31,12 +26,10 @@ function getUserCount({ name, gender }) {
   // const { userCount } = db.prepare('SELECT COUNT(Id) AS userCount FROM users').get();
   const { userCount } = db.prepare(`
     SELECT COUNT(Id) AS userCount
-      FROM users
-      WHERE name LIKE ?
-     `).get(`%${name}%`);
-  //  `).get(name);
-  // WHERE name LIKE '%?%'
-  //  AND gender = ?
+    FROM users
+    WHERE name LIKE ?
+    AND gender LIKE ?
+     `).get(`%${name}%`, `%${gender}%`);
 
   console.log('usreModel.js → getUserCount() 안');
   console.log('userCount: ', userCount);
