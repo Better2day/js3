d46180dab715fe803729680019c932add8200067
Joon <wushu80@hanmail.net>
2026-01-07 04:56:04 +0900

    homework(login) - fetch(json) + in-memory DB

---
+++ 3.EXPRESS/4.SESSION/3.login/public/hw-login2.js
@@ -0,0 +1,22 @@
+// button type='submit' 이 아니라 type='button'을 사용하면 클릭했을 때 자동으로 form이 제출되지 않는다.
+// 그러므로 버튼을 클릭했을 때 preventDefault()를 할 필요도 없다. (발생 이벤트도 'submit' 대신에 'click')
+// 폼 기본 제출 기능이 아니라 fetch로 데이터를 전송할 것이면 type='button'을 사용하는 게 나을 것 같다.
+// document.getElementById('login-form').addEventListener('submit', e => {
+
+document.getElementById('login-form').querySelector('button').addEventListener('click', e => {
+  // e.preventDefault();
+  // console.log('여기');
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username, password })
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
+});
