57478e68c3883a9b5f5ae3b61e6267339817eb5a
Joon <wushu80@hanmail.net>
2026-01-07 05:00:25 +0900

    homework(login) - fetch(json) + session
    - manage session with in-memory DB

---
+++ 3.EXPRESS/4.SESSION/3.login/hw-server3.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const PORT = 3000;
+
+const users = [
+  { id: 1, username: 'u1', password: 'pw1' },
+  { id: 1, username: 'u2', password: 'pw2' }
+];
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(session({
+  secret: 'hw-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 30000
+  }
+}));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'hw-login3.html'));
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(u => u.username == username && u.password == password);
+
+  if (user) {
+    req.session.user = user;
+    res.json({ id: user.id, username: user.username, isLoggedIn: true });
+  } else {
+    res.status(401).json({ message: 'login 실패', isLoggedIn: false });
+  }
+});
+
+app.get('/check-login', (req, res) => {
+  const { user } = req.session;
+
+  if (user) {
+    res.json({ id: user.id, username: user.username, isLoggedIn: true });
+  } else {
+    res.status(401).json({ message: 'login 실패', isLoggedIn: false });
+  }
+});
+
+app.get('/profile', (req, res) => {
+  // if (req.session.) {}
+  const { user } = req.session;
+  console.log(user);
+  if (user) {
+    return res.json({ id: user.id, username: user.username });
+  }
+  res.status(401).json({ error: '회원이 아닙니다.' });
+});
+
+app.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    return res.json({ message: 'logout 성공', isLoggedIn: false })
+  }
+  res.json({ message: 'logout 실패', isLoggedIn: true })
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ 3.EXPRESS/4.SESSION/3.login/public/hw-login3.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('이거 왜 안 돼');
+  checkLogin();
+});
+
+function checkLogin() {
+  fetch('/check-login')
+    .then(res => res.json())
+    .then(data => {
+      console.log('checkLogin 함수 안');
+      setDiv(data);
+    });
+}
+
+function setDiv(data) {
+  const loginDiv = document.getElementById('login-div');
+  const profileDiv = document.getElementById('profile-div');
+
+  if (data && data.isLoggedIn) {
+    loginDiv.style = 'display:none';
+    profileDiv.style = 'display:block';
+    document.getElementById('user-info').innerHTML = `${data.username}님이 로그인 중`;
+  } else {
+    loginDiv.style = 'display:block';
+    profileDiv.style = 'display:none';
+  }
+};
+
+// button type='submit' 이 아니라 type='button'을 사용하면 클릭했을 때 자동으로 form이 제출되지 않는다.
+// 그러므로 버튼을 클릭했을 때 preventDefault()를 할 필요도 없다. (발생 이벤트도 'submit' 대신에 'click')
+// 폼 기본 제출 기능이 아니라 fetch로 데이터를 전송할 것이면 type='button'을 사용하는 게 나을 것 같다.
+// document.getElementById('login-form').addEventListener('submit', e => {
+// e.preventDefault();
+
+document.getElementById('login-form').querySelector('button').addEventListener('click', e => {
+  // console.log('여기');
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username, password })
+  })
+    .then(res => res.json())
+    .then(data => {
+      setDiv(data)
+    });
+});
+
+document.getElementById('logoutBtn').addEventListener('click', () => {
+  fetch('/logout')
+    .then(res => res.json())
+    .then(data => {
+      setDiv(data)
+    });
+})
