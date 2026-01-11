a237528aeec209ad2038274631d118b8f9286373
Joon <wushu80@hanmail.net>
2026-01-12 04:36:23 +0900

    chore(useEffect): remove unnecessary comments

--- 6.REACT/13.useeffect-bootstrap/src/App.jsx
+++ 6.REACT/13.useeffect-bootstrap/src/App.jsx
@@ -3,21 +3,12 @@ import UserCard from './components/UserCard';
 import SearchInput from './components/SearchInput';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  // const countIncrement = (count) => {
-  //   console.log('count: ', count);
-  //   setCount(count + 1);
-  //   console.log('count: ', count);
-  // };
-
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [keyword, setKeyword] = useState('');
 
   useEffect(() => {
     // 변경이 감지되었을 때 할 일을 정의
-    // console.log('진짜 변경이 이루어지는 시점의 count: ', count);
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
       .then(data => {
@@ -25,22 +16,14 @@ function App() {
         setLoading(false);
       })
   }, []);
-  // .then(data => console.log(data))
-  // }, [count]);
 
   if (loading) return <p>로딩중...</p>;
-  // setTimeout(() => {
-  //  if (loading) return <p>로딩중...</p>; }, 2000); // 내 맘대로 해본 것이고 작동하지 않는 듯 한데, 수업중이라서 보류
 
   function removeUser(id) {
     setUsers(users.filter(u => u.id !== id));
   }
 
   const filteredUsers = users.filter(u => u.name.toLowerCase().includes(keyword.toLowerCase()));
-
-  // const getUserInput = (e) => {
-  //   setKeyword(e.target.value);
-  // };
 
   return (
     <>
@@ -56,19 +39,10 @@ function App() {
               <UserCard user={u} onRemove={removeUser} />
             </div>
             // <UserCard key={u.id} user={u} />
-            // <UserCard user={u}  />
           )
-            // }
           )}
-          {/* </ul> */}
         </div>
       </div>
-
-      {/* <div>
-        <button onClick={countIncrement}>
-          count is {count}
-        </button>
-      </div> */}
     </>
   )
 }
