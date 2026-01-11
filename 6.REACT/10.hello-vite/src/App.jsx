b01094b441778e961bc475476a0fa91f62caac9a
Joon <wushu80@hanmail.net>
2026-01-11 17:54:31 +0900

    fix(onClick): move handler logic into function

--- 6.REACT/10.hello-vite/src/App.jsx
+++ 6.REACT/10.hello-vite/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +11,9 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}> */}
+        {/* <button onClick={() => setCount(count + 1)}> */}
+        <button onClick={() => countIncrement(count)}>
           count is {count}
         </button>
       </div>
