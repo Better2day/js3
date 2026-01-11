7a6ac990c3a9a24f48a17b30cc35430ba2c5f848
Joon <wushu80@hanmail.net>
2026-01-12 05:55:57 +0900

    chore(cleanup): add comments

--- 6.REACT/14.useeffetct-debounce/src/App.jsx
+++ 6.REACT/14.useeffetct-debounce/src/App.jsx
@@ -25,7 +25,11 @@ function App() {
         })
     }, 1000); // 1.0초 뒤에 실행
 
-    // 이전에 timeout 설정한 것. 그 다음 useEffect 실행시 그 전의 useEffect를 cleanup 하는 함수
+    // 이전에 timeout을 설정한 것. 그 다음 useEffect 실행시 그 전의 useEffect를 cleanup 하는 함수
+    // (1초가 지나지 않아서 아직 실행되지 않은 기존 setTimeout을 취소시키고, 새 setTimeout을 실행 →
+    // 즉, 입력 후 1초가 지나야 실행되는 debouncing 효과)
+    // react useEffect 규칙: useEffect의 콜백 함수는 cleanup 함수를 반환하거나 아무것도 반환하지 않아야 한다.
+    // cleanup 함수를 반환하면, 다음 effect 실행 전에 react가 cleanup 함수를 호출한다.
     return () => clearTimeout(timer);
 
   }, [keyword]);
