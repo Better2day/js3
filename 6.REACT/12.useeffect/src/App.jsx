03c76bb709748abff0ab7c9a1b197ab04d73e6e2
Joon <wushu80@hanmail.net>
2026-01-12 04:24:26 +0900

    chore(useEffect): remove unnecessary comments

--- 6.REACT/12.useeffect/src/App.jsx
+++ 6.REACT/12.useeffect/src/App.jsx
@@ -2,37 +2,34 @@ import { useState, useEffect } from 'react' // useEffect는 side-effect에서 �
 import UserCard from './components/UserCard';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  // const countIncrement = (count) => {
-  //   console.log('count: ', count);
-  //   setCount(count + 1);
-  //   console.log('count: ', count);
-  // };
-
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // 변경이 감지되었을 때 할 일을 정의
+    setLoading(true);
     // console.log('진짜 변경이 이루어지는 시점의 count: ', count);
+    // side-effect 작업을 하고 싶으면 useEffect 안에서 해야 함
+    // setTimeout(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
       .then(data => {
         setUsers(data);
         setLoading(false);
       })
+    // }, 500);
   }, []);
   // .then(data => console.log(data))
   // }, [count]);
 
   if (loading) return <p>로딩중...</p>;
+
+  // 여기서 setTimeout을 하면 안 됨
   // setTimeout(() => {
-  //  if (loading) return <p>로딩중...</p>; }, 2000); // 내 맘대로 해본 것이고 작동하지 않는 듯 한데, 수업중이라서 보류
+  //   if (loading) return <p>로딩중...</p>;
+  // }, 2000); // 내 맘대로 해본 것이고 작동하지 않는 듯 한데, 수업중이라서 보류
 
   function removeUser(id) {
     setUsers(users.filter(u => u.id !== id));
-
   }
 
   return (
@@ -42,18 +39,12 @@ function App() {
       <div style={{ padding: 16, maxWidth: 500 }}>
         <ul style={{ listStyle: 'none', padding: 0 }}>
           {users.map(u => (
-            // <UserCard key={u.id} user={u} />
             <UserCard key={u.id} user={u} onRemove={removeUser} />
+            // <UserCard key={u.id} user={u} />
             // <UserCard user={u}  />
           ))}
         </ul>
       </div>
-
-      {/* <div>
-        <button onClick={countIncrement}>
-          count is {count}
-        </button>
-      </div> */}
     </>
   )
 }
